1b822f280dbd23b51d09a84615a451e4430deb05
mobile4gamer <153297908+mobile4gamer@users.noreply.github.com>
2023-12-22 10:31:23 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,6 @@ module.exports = {
       sourceType: 'module',
     },
     rules: {
-      // Projenize özel TypeScript kurallarını buraya ekleyebilirsiniz
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
